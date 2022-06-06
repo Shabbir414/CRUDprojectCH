860c61d6b822778a35f7eeca21894bf8fe9c33c5
Shabbir414 <96943187+Shabbir414@users.noreply.github.com>
2022-06-06 12:48:55 +0500

    adding fontawesome

--- inotebook/src/components/Home.js
+++ inotebook/src/components/Home.js
@@ -1,42 +1,37 @@
 import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const { notes, setNotes } = context;
   return (
     <div>
-      <h2>Add a Note</h2>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputPassword" class="form-label">
-            password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          ></input>
-        </div>
-      </form>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputPassword" class="form-label">
-            password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          ></input>
-        </div>
-      </form>
       <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note) => {
-          return note.title;
-        })}
+        <h2>Add a Note</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword" className="form-label">
+              password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+        </form>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword" className="form-label">
+              password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+        </form>
       </div>
+      <Notes />
     </div>
   );
 };
