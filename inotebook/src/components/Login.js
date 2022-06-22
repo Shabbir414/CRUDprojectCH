a1fc0580dc7d1db0c0d017e7565b36dc16452fa3
Shabbir414 <96943187+Shabbir414@users.noreply.github.com>
2022-06-22 14:31:34 +0500

    login complete

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const Login = () => {
+const Login = (props) => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
+  let history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,6 +19,13 @@ const Login = () => {
     });
     const json = await response.json();
     console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem("token", json.authtoken);
+      history.push("/");
+    } else {
+      alert("Invalid credentials");
+    }
   };
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
@@ -51,8 +60,8 @@ const Login = () => {
             className="form-control"
             value={credentials.password}
             onChange={onChange}
-            id="password"
             name="password"
+            id="password"
           ></input>
         </div>
         <button type="submit" className="btn btn-primary">
